657b4547ab4e8615637b54298786c7cdcd17bbde
nicasas <n.casasi@uniandes.edu.co>
2024-05-02 11:45:20 -0500

    feat: iv generador aleatorio

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.crypto.spec.IvParameterSpec;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,7 +6,19 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-
+        System.out.println("");
+        IvParameterSpec iv = GeneratorIv.generateIv();
+        System.out.print("Generated IV (Hex): ");
+        for (byte b : iv.getIV()) {
+            System.out.format("%02x", b); 
+        }
+        System.out.println("");
+        System.out.print("Generated IV (Decimal): ");
+        for (byte b : iv.getIV()) {
+            System.out.print((b & 0xFF) + " "); 
+        }
+        System.out.println(); 
+        System.out.println("Hola");
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
